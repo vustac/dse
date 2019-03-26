ab9df34d31dc918d088579c78f9044b0b1712e9b
Dan McDermet <dan.mcdermet@vanderbilt.edu>
2019-03-26 15:20:43 -0700

    cleanup

--- tests/edu/vanderbilt/isis/threads/simple/check_result.sh
+++ tests/edu/vanderbilt/isis/threads/simple/check_result.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 TESTNAME="ThreadTest"
-expname="x"
-expvalue="16"
 
 echo "Debug info: ${TESTNAME} database entry"
 mongo mydb --quiet --eval 'db.dsedata.find({}, {_id:0})'
 value=`mongo mydb --quiet --eval 'db.dsedata.find({}, {_id:0})' | jq -r '.solution[0].value'`
 name=`mongo mydb --quiet --eval 'db.dsedata.find({}, {_id:0})' | jq -r '.solution[0].name'`
 
-# there may be additional answers (out-of-bounds) so just verify one of the answers matches
+# single anwser
+expname="x"
+expvalue="16"
+
 if [ "${name}" == "${expname}" ] && [ "${value}" == "${expvalue}" ]; then
   echo "${TESTNAME} passed!";
   mongo mydb --quiet --eval 'db.dsedata.deleteMany({})'
