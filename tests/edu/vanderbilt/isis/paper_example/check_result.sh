2733db6457486b455c84fcd41989a9c6dc785762
Daniel Balasubramanian <daniel.a.balasubramanian@vanderbilt.edu>
2019-04-03 11:57:31 -0700

    Update nano test

--- tests/edu/vanderbilt/isis/paper_example/check_result.sh
+++ tests/edu/vanderbilt/isis/paper_example/check_result.sh
@@ -15,7 +15,7 @@ curl -d "a=1&b=0" http://localhost:${SERVERPORT}
 
 # get solver response and check against expected solution
 echo "Debug info: ${TESTNAME} database entry"
-check_single_solution "b" "0"
+check_single_solution "a" "0"
 retcode=$?
 
 # terminate process (this one doesn't auto-terminate)
