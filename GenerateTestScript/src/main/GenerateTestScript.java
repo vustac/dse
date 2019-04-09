7c1618e4f83e292c735f8a4e57d34b7632ccf1f6
Dan McDermet <dan.mcdermet@vanderbilt.edu>
2019-04-09 15:48:21 -0700

    properties change to GenerateTestScript

--- GenerateTestScript/src/main/GenerateTestScript.java
+++ GenerateTestScript/src/main/GenerateTestScript.java
@@ -1,14 +1,12 @@
-package main;
-
-
-import java.io.File;
-import util.Utils;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package main;
+
+import java.io.File;
+import util.Utils;
 
 /**
  *
